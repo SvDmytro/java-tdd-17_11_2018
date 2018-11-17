6e6a2e7ba6aa39229589132aaa26dcee76347571
apanufnik <andrzej.panufnik@gmail.com>
2018-11-17 13:50:08 +0100

    implement addProduct

--- src/main/java/fridge/FridgeService.java
+++ src/main/java/fridge/FridgeService.java
@@ -1,13 +1,18 @@
 package fridge;
 
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 public class FridgeService {
+
+    private final Set<Product> products = new HashSet<>();
+
     Collection<Product> listItems() {
-        return new ArrayList<Product>();
+        return new HashSet<>(products);
     }
 
     void addProduct(final Product product) {
+        products.add(product);
     }
 }
