d5b8a77d298a756cc19b8b28ea8eab0fdb714bd8
Rafal Dziedzic <rafal.dziedzic@ag.pl>
2018-11-17 14:15:36 +0100

    init

--- src/main/java/SugestieZdrowotne.java
+++ src/main/java/SugestieZdrowotne.java
@@ -1,3 +1,70 @@
+import java.util.List;
+
 public class SugestieZdrowotne {
+//    public String zwrocSugestieZdrowotne() {
+//        return "Twoje zdrowie jest OK !";
+//    }
+
+    public String returnSuggestionsForSugars() {
+        int wegle;
+
+        List<Product> list = new List<Product>();
+        int size = list.size();
+
+        int sumWegli = 0;
+        for (Product:
+             list) {
+            int i = 0;
+            int wegle = list.get(i).getWegle();
+            i++;
+
+            int sumWegli +=wegle;
+        }
+        int iloscProduktow = size + 1;
+        int sredniaWegli = sumWegli / iloscProduktow;
+        if (sredniaWegli > 33) {
+            return "Kup więcej bialka lub tluszczy!";
+        }
+
+    }
+
+    public String returnSuggestionsForFat() {
+        int tluszcze;
+        List<Product> list = new List<Product>();
+        int size = list.size();
+
+        for (Product:
+             list) {
+            int i = 0;
+            int tluszcze = list.get(i).getTluszcze();
+            i++;
+
+            int sumTluszczy +=tluszcze;
+        }
+        int iloscProduktow = size + 1;
+        int sredniaTluszczy = sumaTluszczy / iloscProduktow;
+        if (sredniaTluszczy > 33) {
+            return "Kup więcej bialka lub wegli!";
+        }
+    }
+
+    public String returnSuggestionsForProtein() {
+        int bialko;
+        List<Product> list = new List<Product>();
+        int size = list.size();
+
+        for (Product:
+             list) {
+            int i = 0;
+            int bialko = list.get(i).getBialko();
+            i++;
+            int sumBialka +=bialko;
+        }
+        int iloscProduktow = size + 1;
+        int sredniaBialka = sumaBialka / iloscProduktow;
+        if (sredniaBialka > 33) {
+            return "Kup więcej tluszczy lub wegli!";
+        }
+    }
 
 }
--- src/test/java/SugestieZdrowotneTest.java
+++ src/test/java/SugestieZdrowotneTest.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
 
 public class SugestieZdrowotneTest {
+
+    @Test
+    public void suggesterShouldReciveMacro() {
+
+        Assert.assertNotNull(sumBialko);
+        Assert.assertNotNull(sumWegle);
+        Assert.assertNotNull(sumTluszcze);
+    }
+
+    @Test
+    public void suggesterShouldReturnSuggestionForSugars() {
+        SugestieZdrowotne sugestieZdrowotne = new SugestieZdrowotne();
+
+        sugestieZdrowotne.zwrocSugestieZdrowotne();
+        List<MockProduct> mockList = new List<MockProduct>();
+        //TODO
+
+    }
 }
