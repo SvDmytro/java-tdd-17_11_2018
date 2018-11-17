dc071fffaa462a9dd804135ffeecafc3818ede9a
pafnucy <andrzej.panufnik@gmail.com>
2018-11-17 14:34:29 +0100

    change Product equals & hashcode

--- src/main/java/fridge/Product.java
+++ src/main/java/fridge/Product.java
@@ -1,6 +1,8 @@
 package fridge;
 
 
+import java.util.Objects;
+
 // abstract product
 public class Product {
     private String name;
@@ -22,6 +24,19 @@ public class Product {
 
     void setName(final String name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
 
